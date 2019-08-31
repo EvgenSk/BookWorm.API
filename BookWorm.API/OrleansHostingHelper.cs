b9e1e21f12394e2f3ad9777b2bcc03df8dfe7fb5
EvgenSk <evgensk@gmail.com>
2019-08-31 17:52:47 +0700

    configure SiloHostBuilder

--- BookWorm.API/OrleansHostingHelper.cs
+++ BookWorm.API/OrleansHostingHelper.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using NLP.API.Core;
 using Orleans;
 using Orleans.Configuration;
@@ -16,41 +17,52 @@ namespace BookWorm.API
 {
 	public static class OrleansHostingHelper
 	{
-		private static ISiloHost BuildSiloHost(IConfiguration configuration)
-		{
-			var cacheName = "Cache";
-			var storageName = "Storage";
-			var compoundName = "Compound";
+        static string cacheName = "Cache";
+        static string storageName = "Storage";
+        static string compoundName = "Compound";
 
-			return new SiloHostBuilder()
-				.Configure<RedisGrainStorageOptions>(configuration.GetSection("RedisGrainStorage"))
-				.Configure<MongoDBGrainStorageOptions>(configuration.GetSection("MongoDBGrainStorage"))
-				.Configure<WordsAPIOptions>(configuration.GetSection("WordsAPI"))
-				.Configure<StanfordNLPClientOptions>(configuration.GetSection("StanfordNLPClient"))
-				.UseDashboard()
-				.UseLocalhostClustering()
-				.AddRedisGrainStorage(cacheName)
-				.AddMongoDBGrainStorage(storageName)
-				.AddCompoundGrainStorage(compoundName, c => {
-					c.CacheName = cacheName;
-					c.StorageName = storageName;
-				})
-				.AddWordsAPIClient()
-				.AddStanfordNLPClient()
-				.ConfigureApplicationParts(parts =>
-				{
-					parts.AddApplicationPart(typeof(ISimple).Assembly).WithReferences();
-					parts.AddApplicationPart(typeof(SimpleGrain).Assembly).WithReferences();
-				})
-				.Build();
+        private static ISiloHost BuildSiloHost(IConfiguration configuration)
+		{
+            var redisOptionsSection = configuration.GetSection("RedisGrainStorage");
+            var mongoOptionsSection = configuration.GetSection("MongoDBGrainStorage");
+            var wordsAPIOptionsSection = configuration.GetSection("WordsAPI");
+            var stanfordNLPOptionsSection = configuration.GetSection("StanfordNLPClient");
+
+            return new SiloHostBuilder()
+                .ConfigureServices((hostBuilderContext, services) => {
+                    hostBuilderContext.Configuration = configuration;
+                    services
+                    .Configure<RedisGrainStorageOptions>(cacheName, redisOptionsSection)
+                    .Configure<MongoDBGrainStorageOptions>(storageName, mongoOptionsSection);
+                })
+                .Configure<RedisGrainStorageOptions>(redisOptionsSection)
+                .Configure<MongoDBGrainStorageOptions>(mongoOptionsSection)
+                .Configure<WordsAPIOptions>(wordsAPIOptionsSection)
+                .Configure<StanfordNLPClientOptions>(stanfordNLPOptionsSection)
+                .UseDashboard()
+                .UseLocalhostClustering()
+                .AddRedisGrainStorage(cacheName)
+                .AddMongoDBGrainStorage(storageName)
+                .AddCompoundGrainStorage(compoundName, c => {
+                    c.CacheName = cacheName;
+                    c.StorageName = storageName;
+                })
+                .AddWordsAPIClient()
+                .AddStanfordNLPClient()
+                .ConfigureApplicationParts(parts =>
+                {
+                    parts.AddApplicationPart(typeof(ISimple).Assembly).WithReferences();
+                    parts.AddApplicationPart(typeof(SimpleGrain).Assembly).WithReferences();
+                })
+                .Build();
 		}
 
 		public static async Task<IServiceCollection> AddOrleansClusterClient(this IServiceCollection services, IConfiguration configuration)
 		{
-			ISiloHost siloHost = BuildSiloHost(configuration);
-			await siloHost.StartAsync();
+            ISiloHost siloHost = BuildSiloHost(configuration);
+            await siloHost.StartAsync();
 			var client = siloHost.Services.GetRequiredService<IClusterClient>();
-			return 
+			return
 				services
 				.AddSingleton<ISiloHost>(siloHost)		// in order to be disposed correctly
 				.AddSingleton<IClusterClient>(client)
